54b234cc51527801c6e84df082ad22da0976ca8c
serbezov7 <serbezov7@gmail.com>
2022-08-23 19:36:37 +0300

    Exercise

---
+++ First_steps_in_coding/first_steps_in_codind_exercise/basketball_equipment_08.py
@@ -0,0 +1,9 @@
+annual_tax = int(input())
+
+shoes = annual_tax * 0.6
+clothes = shoes * 0.8
+ball = clothes * 0.25
+basketball_acc = ball * 0.2
+
+all_cost = annual_tax + shoes + clothes + ball + basketball_acc
+print(all_cost)
