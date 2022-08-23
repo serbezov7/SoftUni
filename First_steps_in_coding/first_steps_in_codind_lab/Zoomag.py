a9f498d58cc3a35d411e7a1dbbedc4a060532df7
serbezov7 <serbezov7@gmail.com>
2022-08-23 19:35:59 +0300

    Exercise

---
+++ First_steps_in_coding/first_steps_in_codind_lab/Zoomag.py
@@ -0,0 +1,7 @@
+packages_dog_foot = int(input())
+packages_cat_foot = int(input())
+dog_food = float(2.50)
+cat_food = float(4.0)
+total = (packages_dog_foot * dog_food) + (packages_cat_foot * cat_food)
+print(f"{total} lv.")
+
