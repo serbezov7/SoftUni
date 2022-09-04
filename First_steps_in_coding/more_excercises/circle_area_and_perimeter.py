60ba52db21a6929679dbc62ed2fddb0f6ed6a502
serbezov7 <serbezov7@gmail.com>
2022-09-04 22:18:20 +0300

    Exercise

---
+++ First_steps_in_coding/more_excercises/circle_area_and_perimeter.py
@@ -0,0 +1,7 @@
+from math import pi
+
+r = float(input())
+calculated_area = pi * r ** 2
+calculated_parameter = 2 * pi * r
+print("{:.2f}".format(calculated_area))
+print("{:.2f}".format(calculated_parameter))
