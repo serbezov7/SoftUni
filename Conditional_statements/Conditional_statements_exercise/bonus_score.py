cfd34cad12b48cc2522bc862d884db9d14a1a32a
serbezov7 <serbezov7@gmail.com>
2022-09-18 12:53:44 +0300

    Exercise

---
+++ Conditional_statements/Conditional_statements_exercise/bonus_score.py
@@ -0,0 +1,17 @@
+number = float(input())
+bonus = 0
+
+if number <= 100:
+    bonus = 5
+elif number <= 1000:
+    bonus = number * 0.2
+else:
+    bonus = number * 0.1
+
+if number % 2 == 0:
+    bonus = bonus + 1
+elif number % 10 == 5:
+    bonus = bonus + 2
+print(bonus)
+print(bonus + number)
+
