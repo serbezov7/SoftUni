9ee35109901a51944812a731318c276d05eaf269
serbezov7 <serbezov7@gmail.com>
2022-10-28 14:50:57 +0300

    Exercise

---
+++ Programming_Basics/Conditional_statements_advanced/Conditional_statements_advanced_exercise/operations_between_numbers.py
@@ -0,0 +1,38 @@
+first_number = int(input())
+second_number = int(input())
+operator = input()
+result = 0
+even_or_odd = ""
+if operator == "+":
+    result = first_number + second_number
+    if result % 2 == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+elif operator == "-":
+    result = first_number - second_number
+    if result % 2 == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+elif operator == "*":
+    result = first_number * second_number
+    if result % 2 == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+
+if operator == "+" or operator == "-" or operator == "*":
+    print(f"{first_number} {operator} {second_number} = {result} - {even_or_odd}")
+elif operator == "/":
+    if second_number == 0:
+        print(f"Cannot divide {first_number} by zero")
+    else:
+        result = first_number / second_number
+        print(f"{first_number} / {second_number} = {result:.2f}")
+elif operator == "%":
+    if second_number == 0:
+        print(f"Cannot divide {first_number} by zero")
+    else:
+        result = first_number % second_number
+        print(f"{first_number} % {second_number} = {result}")
