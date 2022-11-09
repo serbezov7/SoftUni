08f6fde48d58cc5ef9878483566c4710b3713a9b
serbezov7 <serbezov7@gmail.com>
2022-11-09 20:09:55 +0200

    Exercise

---
+++ Programming_Basics/For_loop/For_loop_lab/number_sequence.py
@@ -0,0 +1,14 @@
+import sys
+
+numbers = int(input())
+max_number = - sys.maxsize
+min_number = sys.maxsize
+
+for number in range(numbers):
+    current_number = int(input())
+    if current_number > max_number:
+        max_number = current_number
+    if current_number < min_number:
+        min_number = current_number
+print(f"Max number: {max_number}")
+print(f"Min number: {min_number}")
